595f75231d0e06b1e41275ad2d14776dd25b35c2
Biven21 <igbochkarev2017@gmail.com>
2021-05-29 12:29:36 +0500

    создал файл, начал ввод программы

---
+++ Boll_Bochkarev.cpp
@@ -0,0 +1,22 @@
+
+
+#include "TXLib.h"
+
+void Move_Boll ();
+
+int main()
+    {
+    txCreateWindow (800, 600);
+
+    Move_Boll ()
+
+    return 0;
+    }
+
+void Move_Boll ()
+    {
+    int x = 100, y = 100,
+
+    While (...)
+        txCircl (
+    }
